2db7d1da76b6735267cb37bb7442396ff542e888
mxx <mlyn@tut.by>
2017-11-26 13:28:50 +0300

    cleaned from unnecessary comments

--- simpleblog/forms.py
+++ simpleblog/forms.py
@@ -8,27 +8,6 @@ from django.forms import widgets
 
 from simpleblog.models import Comment, Post
 
-# def username_is_not_digit(username):
-#     if username.isdigit():
-#         raise ValidationError('Username can not be entirely numeric')
-#
-# class SignUpForm(UserCreationForm):
-#     email = EmailField(max_length=200, help_text='Required', required=True)
-#     username = CharField(max_length=150,
-#             help_text="<ul><li>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</li> \
-#                         <li>Your username can't be entirely numeric.</li></ul>", required=True, validators=[username_is_not_digit])
-#
-#     # email should be unique
-#     def clean_email(self):
-#         data = self.cleaned_data['email']
-#         if User.objects.filter(email=data).exists():
-#             raise ValidationError("This email already used")
-#         return data
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'first_name', 'last_name', )
-
 
 class PostEditForm(ModelForm):
 
