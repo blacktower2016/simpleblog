d955b8f244e64dd10bd46ff39f21031bc94ee98a
mxx <mlyn@tut.by>
2017-11-26 12:52:25 +0300

    simpleblog formas are ready for translation

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,22 +7,24 @@ from django.contrib.auth.models import User
 
 from simpleblog.models import Comment, Post
 
+from django.utils.translation import gettext as _
+
 def username_is_not_numeric(username):
     if username.isdigit():
-        raise ValidationError('Username can not be entirely numeric')
+        raise ValidationError(_('Username can not be entirely numeric'))
 
 class SignUpForm(UserCreationForm):
     email = EmailField(max_length=200, help_text='Required', required=True)
     username = CharField(max_length=150,
-            help_text="<ul><li>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</li> \
-                        <li>Your username can't be entirely numeric.</li></ul>",
+            help_text=_("<ul><li>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</li> \
+                        <li>Your username can't be entirely numeric.</li></ul>"),
                          required=True, validators=[username_is_not_numeric])
 
     # email should be unique
     def clean_email(self):
         data = self.cleaned_data['email']
         if User.objects.filter(email=data).exists():
-            raise ValidationError("This email already used")
+            raise ValidationError(_("This email already used"))
         return data
 
     class Meta:
