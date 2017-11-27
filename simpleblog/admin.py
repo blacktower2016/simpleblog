d9143f2046b17b6ba801e599c9abfc6f8d7ba105
mxx <mlyn@tut.by>
2017-11-27 10:21:13 +0300

    add sorting by tagged posts number in Tag admin panel

--- simpleblog/admin.py
+++ simpleblog/admin.py
@@ -3,15 +3,30 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Comment, Tag
 
+from django.db.models import Count
+
 class CommentInline(admin.TabularInline):
     model = Comment
     extra = 0
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['tag_name', 'tag_posts_count']
-    def tag_posts_count(self, obj):
-        return obj.post_set.count()
+
+    list_display = ['tag_name', 'posts_number']
+
+    # add sorting by posts number
+    def get_queryset(self, *args, **kwargs):
+        """Annotate tags with number of posts"""
+        return super().get_queryset(*args, **kwargs).annotate(posts_count=Count('post'))
+
+    # add new field with tag number of posts
+    def posts_number(self, obj):
+        return obj.posts_count
+
+    # make this field sortable
+    posts_number.admin_order_field = 'posts_count'
+
+
 
 class TagInline(admin.StackedInline):
     model = Tag
